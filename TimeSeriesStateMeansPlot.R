e6d329d434a4f630a3b0d7f7f0c0c8266a3ab716
jackcarter23 <Jack@MacBook-Pro-74.local>
2017-03-06 17:55:10 +0000

    function to plot the time series with means from most likely state

---
+++ TimeSeriesStateMeansPlot.R
@@ -0,0 +1,10 @@
+# function for plotting the time series of the data with the mean of the observation density from the most likely state
+# data_analysis is the time series data with Time and Activity variables
+# HMMpost is the posterior probabilities of each state at each time point. Needs a variable state which gives the most likely state at each time point.
+# summaryfm is the summary of the fitted model. This should be a matrix with the means of the observation distributions for each state in the first column.
+
+TimeSeriesStateMeansPlot <- function(data_analysis, HMMpost, summaryfm) {
+ggplot(cbind(data_analysis,HMMpost),aes(x=Time,y=Activity,group=1)) 
++ geom_line(size=.3) 
++ geom_segment(data=cbind(data_analysis,HMMpost), aes(x=Time,xend=Time+330,y=summaryfm[state,1],yend=summaryfm[state,1],colour=as.factor(state)),size=1.5)
+}
